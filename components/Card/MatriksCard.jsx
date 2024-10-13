9c16e92387c2ec0b70b10f5ae6337f169b98c509
OnyanPokon <mohamadrafiqd@gmail.com>
2024-10-13 20:15:09 +0800

    feat: separated component matriks

---
+++ components/Card/MatriksCard.jsx
@@ -0,0 +1,104 @@
+'use client';
+import { Button, Card, Collapse, message } from 'antd';
+import React from 'react';
+import DataTable from '../DataTable/DataTable';
+import { getByUserIdAndPeriode } from '@/controller/SKPController';
+import { store as storeRHK } from '@/controller/RHKController';
+
+const MatriksCard = ({ SKP, dataItem, rhkData, aspekData, rencanaAksiData, setModal, modal }) => {
+    return (
+        <Card type="inner" title={dataItem.userId}>
+            <div className="grid grid-flow-row divide-y text-xs">
+                <div className="flex items-center justify-between py-2">
+                    <span className="uppercase font-semibold">nama</span>
+                    <p className="text-right uppercase">{dataItem.nama_asn}</p>
+                </div>
+                <div className="flex items-center justify-between py-2">
+                    <span className="uppercase font-semibold">jabatan</span>
+                    <div className="flex flex-col gap-y-2 text-right items-end">
+                        <p>{dataItem.nama_jabatan}</p>
+                        {/* <small>ID : {item.id || '197801012007011026'}</small> */}
+                    </div>
+                </div>
+                <div className="flex flex-col gap-y-4 py-2 pt-4">
+                    <div className="flex items-center gap-x-2">
+                        <Button className="w-fit" type="primary">
+                            Lihat SKP
+                        </Button>
+                    </div>
+                    <Collapse bordered>
+                        <Collapse.Panel
+                            key="1"
+                            header="RHK Yang di Intervensi"
+                            extra={
+                                <Button
+                                    onClick={() =>
+                                        setModal({
+                                            trigger: true,
+                                            title: 'Tambah RHK Intervensi',
+                                            type: 'create',
+                                            formFields: rhkData.fields,
+                                            onSubmit: async (value) => {
+                                                const response = await getByUserIdAndPeriode(dataItem.userId, SKP.periodeRKT);
+                                                console.log(value);
+                                                const skp = response.data;
+                                                console.log(skp);
+
+                                                if (skp) {
+                                                    const dt = {
+                                                        ...value,
+                                                        skp: skp._id
+                                                    };
+                                                    const rhk = await storeRHK(dataItem.userId, dt);
+
+                                                    console.log(rhk);
+
+                                                    message.success('Berhasil Menambahkan RHK');
+                                                } else {
+                                                    const data = {
+                                                        periode_awal: SKP.periode_awal,
+                                                        periode_akhir: SKP.periode_akhir,
+                                                        skp: [SKP._id],
+                                                        periodeRKT: SKP.periodeRKT,
+                                                        pendekatan: SKP.pendekatan,
+                                                        renstra: SKP.renstra,
+                                                        jabatan: [dataItem]
+                                                    };
+                                                    const newSKP = await storeSKP(dataItem.userId, data, '0');
+                                                    const dt = {
+                                                        ...value,
+                                                        skp: newSKP?.data._id
+                                                    };
+
+                                                    const rhk = await storeRHK(dataItem.userId, dt);
+                                                    message.success('Berhasil Menambahkan RHK');
+                                                }
+                                                setModal({trigger: false });
+                                            }
+                                        })
+                                    }
+                                >
+                                    Tambah RHK
+                                </Button>
+                            }
+                        >
+                            <DataTable columns={rhkData.columns} data={rhkData.data} loading={rhkData.loading} />
+                        </Collapse.Panel>
+                        <Collapse.Panel key="2" header="Aspek" extra={<Button onClick={() => setModal({ trigger: true, title: 'Tambah Aspek', type: 'create', formFields: aspekData.fields, onSubmit: () => {} })}>Tambah Aspek</Button>}>
+                            <DataTable columns={aspekData.columns} data={aspekData.data} loading={aspekData.loading} />
+                        </Collapse.Panel>
+                        <Collapse.Panel
+                            key="3"
+                            header="Rencana Aksi"
+                            extra={<Button onClick={() => setModal({ trigger: true, title: 'Tambah Rencana Aksi', type: 'create', formFields: rencanaAksiData.fields, onSubmit: () => {} })}>Tambah Rencana Aksi</Button>}
+                        >
+                            <DataTable columns={rencanaAksiData.columns} data={rencanaAksiData.data} loading={rencanaAksiData.loading} />
+                        </Collapse.Panel>
+                    </Collapse>
+                </div>
+            </div>
+        </Card>
+    );
+};
+
+export default MatriksCard;
