02d572d2d808c5fcf316f0aa12e4b7be65d53a86
OnyanPokon <mohamadrafiqd@gmail.com>
2025-01-21 03:57:15 +0800

    fix kurva

--- app/dashboard/skp/[IdSkp]/periode_penilaian/[IdPeriode]/penilaian/[IdRhk]/lihat_kurva/page.jsx
+++ app/dashboard/skp/[IdSkp]/periode_penilaian/[IdPeriode]/penilaian/[IdRhk]/lihat_kurva/page.jsx
@@ -8,7 +8,7 @@ import Link from 'next/link';
 import React from 'react';
 import { Data, dummyHasilPenilaian } from '@/data/dummyData';
 import { CategoryScale, LinearScale, PointElement, LineElement, Title as ChartTitle, Tooltip, Legend, Chart } from 'chart.js';
-import { DataTable } from '@/components';
+import { DataTable, FilterField } from '@/components';
 Chart.register(CategoryScale, LinearScale, PointElement, LineElement, ChartTitle, Tooltip, Legend);
 
 const { Title } = Typography;
@@ -21,7 +21,7 @@ const page = () => {
 
     useEffect(() => {
         // Prepare data for the chart
-        const labels = Data.map((item) => item.year);
+        const labels = ['Sangat Kurang', 'Kurang', 'Butuh Perbaikan', 'Baik', 'Sangat Baik'];
         const userGains = Data.map((item) => item.userGain);
         const userLosses = Data.map((item) => item.userLost);
 
@@ -29,16 +29,16 @@ const page = () => {
             labels: labels,
             datasets: [
                 {
-                    label: 'User Gain',
+                    label: 'Ideal',
                     data: userGains,
-                    borderColor: '#93c5fd',
+                    borderColor: '#50C878',
                     backgroundColor: 'rgba(147, 197, 253, 0.2)',
                     fill: true
                 },
                 {
-                    label: 'User Lost',
+                    label: 'Realita',
                     data: userLosses,
-                    borderColor: '#fca5a5',
+                    borderColor: '#FF5733',
                     backgroundColor: 'rgba(252, 165, 165, 0.2)',
                     fill: true
                 }
@@ -71,6 +71,94 @@ const page = () => {
       
     ];
 
+    const filterFileds = [
+        {
+            label: 'Tipe',
+            name: 'tipe',
+            type: 'select',
+            options: [
+                {
+                    label: 'Ideal untuk capaian kinerja organisasi istimewa',
+                    value: 'ideal untuk capaian kinerja organisasi istimewa'
+                },
+                {
+                    label: 'Ideal untuk capaian kinerja organisasi baik',
+                    value: 'ideal untuk capaian kinerja organisasi baik'
+                },
+                {
+                    label: 'Ideal untuk capaian kinerja organisasi butuh perbaikan',
+                    value: 'ideal untuk capaian kinerja organisasi butuh perbaikan'
+                },
+                {
+                    label: 'Ideal untuk capaian kinerja organisasi kurang',
+                    value: 'ideal untuk capaian kinerja organisasi kurang'
+                },
+                {
+                    label: 'Ideal untuk capaian kinerja organisasi sangat kurang',
+                    value: 'ideal untuk capaian kinerja organisasi sangat kurang'
+                },
+            ],
+            filter: 'eq'
+        },
+    ];
+
+    const onFilter = async (values) => {
+        filterFileds.forEach((field) => {
+            let value = values[field.name];
+            if (value !== undefined && value !== null) {
+                switch (field.type) {
+                    case 'date':
+                        value = dateFormatter(value);
+                        break;
+
+                    default:
+                        value = value;
+                        break;
+                }
+
+                switch (field.filter) {
+                    case 'gte':
+                        pagination.filters[field.name] = { $gte: value };
+                        break;
+                    case 'lte':
+                        pagination.filters[field.name] = { $lte: value };
+                        break;
+                    case 'gt':
+                        pagination.filters[field.name] = { $gt: value };
+                        break;
+                    case 'lt':
+                        pagination.filters[field.name] = { $lt: value };
+                        break;
+                    case 'eq':
+                        pagination.filters[field.name] = value; // Equality
+                        break;
+                    case 'ne':
+                        pagination.filters[field.name] = { $ne: value };
+                        break;
+                    case 'in':
+                        pagination.filters[field.name] = { $in: Array.isArray(value) ? value : [value] };
+                        break;
+                    case 'nin':
+                        pagination.filters[field.name] = { $nin: Array.isArray(value) ? value : [value] };
+                        break;
+                    case 'regex':
+                        pagination.filters[field.name] = { $regex: value, $options: 'i' }; // Case-insensitive regex
+                        break;
+                    case 'exists':
+                        pagination.filters[field.name] = { $exists: Boolean(value) };
+                        break;
+                    default:
+                        console.warn(`Unsupported filter type: ${field.filter}`);
+                }
+            } else {
+                if (pagination.filters.hasOwnProperty(field.name)) {
+                    delete pagination.filters[field.name];
+                }
+            }
+        });
+        fetchData();
+    };
+
     return (
         <div className="w-full flex flex-col gap-y-4">
             <Breadcrumb
@@ -87,9 +175,12 @@ const page = () => {
                 <div className="flex flex-col gap-y-4">
                     <div className="flex items-center justify-between mb-12">
                         <Title className="mt-2" level={5}>
-                            Data Misi
+                            Data Kurva Penilaian Bawahan
                         </Title>
                     </div>
+                    <div className="w-full">
+                            <FilterField fields={filterFileds} onSubmit={onFilter}></FilterField>
+                        </div>
                     <div className="">
                         <Card>
                             <Line data={penilaianChart} />
