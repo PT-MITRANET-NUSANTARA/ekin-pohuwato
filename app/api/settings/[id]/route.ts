32ed4a37b509542fc17411f46655177080abaf2e
zYuna646 <anggarangga646@gmail.com>
2025-02-13 02:59:00 +0800

    fix

---
+++ app/api/settings/[id]/route.ts
@@ -0,0 +1,45 @@
+import { NextRequest, NextResponse } from 'next/server';
+import { serialize } from 'cookie';
+import jwt from 'jsonwebtoken';
+import { cookies } from 'next/headers';
+import { createResponse } from '@/utils/api';
+import { getByNIP } from '@/controller/IDSN/JabatanController';
+import Settings from '@/models/Settings';
+import dbConnect from '@/utils/db';
+
+export async function GET(req: NextRequest) {
+    await dbConnect();
+
+    try {
+        const settings = await Settings.findOne();
+        return NextResponse.json(createResponse(200, 'Success', settings));
+    } catch (error) {
+        console.error('GET error:', error);
+        return NextResponse.json({ error: 'Failed to fetch data' }, { status: 500 });
+    }
+}
+
+// PUT update Visi
+export async function PUT(req: NextRequest, { params }: { params: { id: string } }) {
+    await dbConnect();
+    try {
+        const { id } = params;
+        const body = await req.json();
+        // const errors = validateVisiData(body);
+
+        // if (errors.length > 0) {
+        //     return NextResponse.json(createResponse(400, 'Failed', errors));
+        // }
+
+        const updateSettings = await Settings.findOneAndUpdate({ _id: id }, body, { new: true });
+
+        if (!updateSettings) {
+            return NextResponse.json(createResponse(404, 'Visi not found', null));
+        }
+
+        return NextResponse.json(createResponse(200, 'Success', updateSettings, true));
+    } catch (error) {
+        console.error('PUT error:', error);
+        return NextResponse.json({ error: 'Failed to update Visi' }, { status: 500 });
+    }
+}
--- app/dashboard/web_settings/page.jsx
+++ app/dashboard/web_settings/page.jsx
@@ -1,44 +1,87 @@
 'use client';
+import { getData } from '@/controller/AuthorizationController';
+import { get, update } from '@/controller/SettingsController';
+import useFetchData from '@/hooks/useFetchData';
 import { Breadcrumb, Card, Form, Tabs, Input, Button, TimePicker, InputNumber } from 'antd';
-import React, { useState } from 'react';
+import dayjs from 'dayjs';
+import React, { useEffect, useState } from 'react';
 
 const page = () => {
-    const [submitLoading, setSubmitLoading] = useState()
-    const submitFinish = (values) => {
-        setSubmitLoading(false)
-        console.log(values)
-        setSubmitLoading(true)
+    const [submitLoading, setSubmitLoading] = useState();
+    const { data: user, setData: setUser } = useFetchData(getData);
+    const [data, setData] = useState(null);
+
+    const submitFinish = async (values) => {
+        setSubmitLoading(false);
+        console.log(values);
+        setSubmitLoading(true);
+        const data = {
+            ...values
+        }
+
+        const res = await update(data._id, data);
+        if (res.ok) {
+            fetchData()
+        }
+    };
+
+    useEffect(() => {
+        if (user) {
+            fetchData();
+        }
+    }, [user]);
+
+    const fetchData = async () => {
+        try {
+            const data = await get();
+            const values = {
+                ...data.data,
+                istirahat_start: dayjs(`${dayjs().format("YYYY-MM-DD")} ${data.data.istirahat_start}`, "YYYY-MM-DD HH:mm:ss"),
+                istirahat_end: dayjs(`${dayjs().format("YYYY-MM-DD")} ${data.data.istirahat_end}`, "YYYY-MM-DD HH:mm:ss"),
+                harian_start: dayjs(`${dayjs().format("YYYY-MM-DD")} ${data.data.harian_start}`, "YYYY-MM-DD HH:mm:ss"),
+                harian_end: dayjs(`${dayjs().format("YYYY-MM-DD")} ${data.data.harian_end}`, "YYYY-MM-DD HH:mm:ss")
+            };
+            console.log(values);
+            
+            setData(values);
+        } catch (error) {
+            console.log(error);
+        }
+    };
+
+    if (!data) {
+        return <></>
     }
     return (
         <div className="w-full flex flex-col gap-y-4">
             <Card className="">
                 <Tabs defaultActiveKey="1" tabPosition="left" className="min-h-screen">
                     <Tabs.Items tab="Admin Setting" key="1">
-                        <Form layout='vertical' className="flex flex-col gap-y-2 mt-6" onFinish={submitFinish}>
-                            <Form.Item name="admin_id" label="Admin ID" className='m-0'>
-                                <Input size='large' placeholder='Masukan admin ID' />
+                        <Form layout="vertical" className="flex flex-col gap-y-2 mt-6" onFinish={submitFinish}>
+                            <Form.Item name="admin_id" label="Admin ID" className="m-0">
+                                <Input size="large" placeholder="Masukan admin ID" />
                             </Form.Item>
-                            <Form.Item name="feedback" label="Total Feedback" className='m-0'>
-                                <InputNumber size='large'  placeholder='masukan total feedback' className='w-full'/>
+                            <Form.Item name="feedback" label="Total Feedback" className="m-0" initialValue={data}>
+                                <InputNumber size="large" placeholder="masukan total feedback" className="w-full" />
                             </Form.Item>
-                            <Form.Item name="time" label="Total Waktu" className='m-0'>
-                                <InputNumber size='large' placeholder='masukan total waktu' className='w-full' />
+                            <Form.Item name="time" label="Total Waktu" className="m-0" initialValue={data}>
+                                <InputNumber size="large" placeholder="masukan total waktu" className="w-full" />
                             </Form.Item>
-                            <Form.Item name="harian_start" label="Harian Mulai" className='m-0'>
-                                <TimePicker size='large' className='w-full' />
+                            <Form.Item name="harian_start" label="Harian Mulai" className="m-0" initialValue={data}>
+                                <TimePicker size="large" className="w-full" />
                             </Form.Item>
-                            <Form.Item name="harian_end" label="Harian Akhir" className='m-0'>
-                                <TimePicker size='large' className='w-full' />
+                            <Form.Item name="harian_end" label="Harian Akhir" className="m-0" initialValue={data}>
+                                <TimePicker size="large" className="w-full" />
                             </Form.Item>
-                            <Form.Item name="istrahat_start" label="Istrahat Mulai" className='m-0'>
-                                <TimePicker size='large' className='w-full' />
+                            <Form.Item name="istrahat_start" label="Istrahat Mulai" className="m-0" initialValue={data}>
+                                <TimePicker size="large" className="w-full" />
                             </Form.Item>
-                            <Form.Item name="istrahat_end" label="Istrahat Mulai" className='m-0'>
-                                <TimePicker size='large' className='w-full' />
+                            <Form.Item name="istrahat_end" label="Istrahat Mulai" className="m-0" initialValue={data}>
+                                <TimePicker size="large" className="w-full" />
                             </Form.Item>
 
-                            <Form.Item className='m-0 mt-6'>
-                                <Button type='primary' htmlType='submit' loading={submitLoading}>
+                            <Form.Item className="m-0 mt-6">
+                                <Button type="primary" htmlType="submit" loading={submitLoading}>
                                     Simpan
                                 </Button>
                             </Form.Item>
